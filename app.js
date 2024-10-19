54d45a12fa763176b78fb3eb96d1ac4eaea230be
HarounaTraore <167339173+HarounaTraore@users.noreply.github.com>
2024-10-19 02:09:57 +0000

    developpement services User

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { config } from "dotenv";
+import helmet from "helmet";
 config();
 const port = 3000;
 const app = express();
 app.use(bodyParser.json());
+app.use(helmet());
+
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  getByIdUser,
+  updateUser,
+} from "./src/services/User.js";
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
---
+++ src/services/User.js
@@ -0,0 +1,72 @@
+import prisma from "../config/prisma.js";
+
+export const getAllUser = async () => {
+  try {
+    const result = await prisma.users.findMany();
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+export const getByIdUser = async (id) => {
+  try {
+    const result = await prisma.users.findUnique({ where: { id } });
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+export const createUser = async (
+  name,
+  email,
+  address,
+  phone,
+  password,
+  role
+) => {
+  try {
+    const result = await prisma.users.create({
+      data: { name, email, address, phone, password, role },
+    });
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const updateUser = async (
+  id,
+  name,
+  email,
+  address,
+  phone,
+  password,
+  role
+) => {
+  try {
+    const result = await prisma.users.update({
+      where: { id },
+      data: { name, email, address, phone, password },
+    });
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+export const deleteUser = async (id) => {
+  try {
+    await prisma.users.delete({ where: { id } });
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+};
